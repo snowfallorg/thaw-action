5be03b010e77fa2d4a5eeea02f173aaa42e7b672
Jake Hamilton <jake.hamilton@hey.com>
2023-11-25 22:29:43 -0800

    chore: initial commit

---
+++ thaw-action.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ -n "$FLAKE_PATH" ]]; then
+	cd "$FLAKE_PATH"
+fi
+
+nix_options=()
+if [[ -n "$NIX_OPTIONS" ]]; then
+	for option in $NIX_OPTIONS; do
+		nix_options+=("${option}")
+	done
+fi
+
+if [[ -n "$TARGET_INPUTS" ]]; then
+	nix "${nix_options[@]}" run "${THAW_URI}" -- "$TARGET_INPUTS"
+else
+	nix "${nix_options[@]}" run "${THAW_URI}"
+fi
+
+if [[ -z "$(git status --porcelain flake.nix flake.lock)" ]]; then
+	echo "No changes to commit"
+	exit 1
+fi
